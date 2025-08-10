3fdc5a0b371f4a3f1583150edea2979fdebe1d51
Tan Phat <nguyenvantanphat.it@gmail.com>
2025-08-10 21:23:48 +0700

    fix animation button

--- components/seaction-contact.tsx
+++ components/seaction-contact.tsx
@@ -17,7 +17,7 @@ export default function SeactionContact() {
                     whileInView={{ opacity: 1, x: 0 }}
                     viewport={{ once: true }}
                     transition={{ duration: 0.6, delay: 0.2 }}
-                    className="flex justify-center items-center md:w-[434px] md:h-[433px] md:shrink-0 "
+                    className="flex justify-center items-center md:w-[434px] md:h-[433px] md:shrink-0"
                 >
                     <Image
                         src="/contact/Amor-spa-contact.png"
@@ -62,18 +62,20 @@ export default function SeactionContact() {
                         >
                             <Link
                                 href="/menu"
-                                className="group relative inline-flex w-[220px] items-center justify-center gap-2 py-3 md:py-4 px-[50px] md:px-10 font-semibold rounded-full whitespace-nowrap border-2 border-[#CC424E] text-[#CC424E] overflow-hidden transition-all duration-300
-              before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-0
-              before:bg-[#CC424E] before:rounded-inherit before:transition-[width] before:ease-in-out before:duration-700
-              hover:before:w-full hover:boder-transparent hover:text-white"
+                                className="group relative inline-flex w-[220px] items-center justify-center gap-2 py-4 px-10 font-semibold rounded-full whitespace-nowrap border-2 border-[#CC424E] text-[#CC424E] overflow-hidden hover:text-white"
+
+
+
                             >
+                                <div className="absolute top-full left-1/2 -translate-x-1/2 w-[200%] h-[200%] rounded-full group-hover:-translate-y-1/2 transition-transform duration-500 ease-in-out z-[-1] bg-[#CC424E]" />
+
                                 <svg
-                                    width="24"
+                                    width="25"
                                     height="24"
-                                    viewBox="0 0 24 24"
+                                    viewBox="0 0 25 24"
                                     fill="none"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    className="relative z-10 fill-current transition-colors duration-300 group-hover:fill-white"
+                                    className="relative z-10 fill-current transition-colors duration-300"
                                 >
                                     <path
                                         fillRule="evenodd"
@@ -81,8 +83,8 @@ export default function SeactionContact() {
                                         d="M3.80888 5.25C4.16846 2.98301 6.13185 1.25 8.5 1.25H18.5C21.1234 1.25 23.25 3.37665 23.25 6V18C23.25 20.6234 21.1234 22.75 18.5 22.75H8.5C6.13185 22.75 4.16846 21.017 3.80888 18.75H2.5C2.08579 18.75 1.75 18.4142 1.75 18C1.75 17.5858 2.08579 17.25 2.5 17.25H3.75V12.75H2.5C2.08579 12.75 1.75 12.4142 1.75 12C1.75 11.5858 2.08579 11.25 2.5 11.25H3.75V6.75H2.5C2.08579 6.75 1.75 6.41421 1.75 6C1.75 5.58579 2.08579 5.25 2.5 5.25H3.80888ZM5.25 6.75H6.5C6.91421 6.75 7.25 6.41421 7.25 6C7.25 5.58579 6.91421 5.25 6.5 5.25H5.33697C5.67555 3.81665 6.96321 2.75 8.5 2.75H18.5C20.2949 2.75 21.75 4.20507 21.75 6V18C21.75 19.7949 20.2949 21.25 18.5 21.25H8.5C6.96321 21.25 5.67555 20.1833 5.33697 18.75H6.5C6.91421 18.75 7.25 18.4142 7.25 18C7.25 17.5858 6.91421 17.25 6.5 17.25H5.25V12.75H6.5C6.91421 12.75 7.25 12.4142 7.25 12C7.25 11.5858 6.91421 11.25 6.5 11.25H5.25V6.75ZM9.75 6C9.75 5.58579 10.0858 5.25 10.5 5.25L18.5 5.25C18.9142 5.25 19.25 5.58579 19.25 6C19.25 6.41421 18.9142 6.75 18.5 6.75L10.5 6.75C10.0858 6.75 9.75 6.41421 9.75 6ZM9.75 10C9.75 9.58578 10.0858 9.25 10.5 9.25L14.5 9.25C14.9142 9.25 15.25 9.58579 15.25 10C15.25 10.4142 14.9142 10.75 14.5 10.75L10.5 10.75C10.0858 10.75 9.75 10.4142 9.75 10Z"
                                     />
                                 </svg>
+                                <span className="relative z-10 text-base font-semibold">Xem Menu</span>
 
-                                <span className="relative z-10 text-base font-semibold">Xem menu</span>
                             </Link>
                         </motion.div>
                         <motion.div
@@ -96,16 +98,20 @@ export default function SeactionContact() {
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 aria-label="Nhắn tin qua Facebook"
-                                className="group relative inline-flex w-[220px] items-center justify-center gap-2 py-3 md:py-4 px-[50px] md:px-10 font-semibold rounded-full whitespace-nowrap border-2 border-[#CC424E] text-[#CC424E]  overflow-hidden transition-all duration-300
-                                before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-0
-                                before:bg-[#CC424E] before:rounded-inherit before:transition-[width] before:ease-in-out before:duration-700
-                                hover:before:w-full hover:border-transparent hover:text-white"
+                                className="group relative inline-flex w-[220px] items-center justify-center gap-2 py-4 px-10 font-semibold rounded-full whitespace-nowrap border-2 border-[#CC424E] text-[#CC424E] overflow-hidden hover:text-white"
+
+
+
                             >
+                                <div className="absolute top-full left-1/2 -translate-x-1/2 w-[200%] h-[200%] rounded-full group-hover:-translate-y-1/2 transition-transform duration-500 ease-in-out z-[-1] bg-[#CC424E]" />
+
                                 <svg
+                                    width="19"
+                                    height="18"
                                     viewBox="0 0 19 18"
                                     fill="none"
                                     xmlns="http://www.w3.org/2000/svg"
-                                    className="relative z-10 w-6 h-6 fill-current transition-colors duration-300 group-hover:fill-white"
+                                    className="relative z-10 fill-current transition-colors duration-300"
                                 >
                                     <path
                                         fillRule="evenodd"
